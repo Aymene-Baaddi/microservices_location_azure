c01b0b5747f551d4008775988b7d9826aae344d2
aymene <aymenebd@gmail.com>
2024-08-01 12:07:22 +0100

    deploy the app

--- micro-front-admin/src/App.js
+++ micro-front-admin/src/App.js
@@ -1,4 +1,8 @@
-import { BrowserRouter as Router, Route ,Routes } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes ,useLocation } from 'react-router-dom';
+import './App.css';
+
+
 import './App.css';
 import Login from './login/login';
 import Home from './pages/home';
@@ -12,25 +16,50 @@ import ShowUser from './pages/user/ShowUser';
 import ShowReservations from './pages/reservation/ShowReservation';
 
 
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const isAuthenticated = localStorage.getItem('isLoggedIn') === 'true';
+    setIsLoggedIn(isAuthenticated);
+    console.log(isLoggedIn)
+  }, []);
 
-function App() {
   return (
     <Router>
-      <Sidebar/>
-    <Routes>
-      <Route path="/home"  element={<Home/>} />
-      <Route path="/addadmin" element={<AddAdmin/>} />
-      <Route path="/cars" element={<ShowCar/>} />
-      <Route path="/updatecar/:id" element={<UpdateCar/>} />
-      <Route path="/addcar" element={<AddCar/>} />
-      <Route path="/adduser" element={<AddUser/>} />
-      <Route path="/users" element={<ShowUser/>} />
-      <Route path="/reservations" element={<ShowReservations/>} />
-      <Route path="/"  element={<Login/>} />
-    </Routes>
-  </Router>
+      <ConditionalNavbar isLoggedIn={isLoggedIn} />
+      <Routes>
+        <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        
+        {isLoggedIn ? (
+          <>
+              <Route path="/home"  element={<Home/>} />
+              <Route path="/addadmin" element={<AddAdmin/>} />
+              <Route path="/cars" element={<ShowCar/>} />
+              <Route path="/updatecar/:id" element={<UpdateCar/>} />
+              <Route path="/addcar" element={<AddCar/>} />
+              <Route path="/adduser" element={<AddUser/>} />
+              <Route path="/users" element={<ShowUser/>} />
+              <Route path="/reservations" element={<ShowReservations/>} />
+               
+
+           
+          </>
+        ) : (
+          <Route path="/" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+        )}
+      </Routes>
+    </Router>
   );
-}
+};
+
+const ConditionalNavbar = ({ isLoggedIn }) => {
+  const location = useLocation();
+
+  const isLoginPage = location.pathname === '/';
+
+
+  return isLoggedIn && !isLoginPage  && <Sidebar/>;
+};
 
 export default App;
